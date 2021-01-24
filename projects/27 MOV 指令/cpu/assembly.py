2acc5a2a8903c0506c776db648ba250cb4caf0fe
Steven Kang <kangweibaby@163.com>
2021-01-24 16:01:52 +0800

    add project 27

---
+++ projects/27 MOV 指令/cpu/assembly.py
@@ -0,0 +1,41 @@
+# coding=utf-8
+
+import pin
+
+FETCH = [
+    pin.PC_OUT | pin.MAR_IN,
+    pin.RAM_OUT | pin.IR_IN | pin.PC_INC,
+    pin.PC_OUT | pin.MAR_IN,
+    pin.RAM_OUT | pin.DST_IN | pin.PC_INC,
+    pin.PC_OUT | pin.MAR_IN,
+    pin.RAM_OUT | pin.SRC_IN | pin.PC_INC,
+]
+
+
+MOV = 0 | pin.ADDR2
+ADD = (1 << pin.ADDR2_SHIFT) | pin.ADDR2
+
+NOP = 0
+HLT = 0x3f
+
+INSTRUCTIONS = {
+    2: {
+        MOV: {
+            (pin.AM_REG, pin.AM_INS): [
+                pin.DST_W | pin.SRC_OUT,
+            ]
+        }
+    },
+    1: {},
+    0: {
+        NOP: [
+            pin.CYC,
+        ],
+        HLT: [
+            pin.HLT,
+        ]
+    }
+}
+
+
+print(bin(MOV))
