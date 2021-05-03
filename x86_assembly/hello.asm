79e4e11724d935e7f9a7a4582afded35834e1822
Steven Kang <kangweibaby@163.com>
2021-05-03 17:28:50 +0800

    x86 汇编语言 - 02 hello world

--- x86_assembly/hello.asm
+++ x86_assembly/hello.asm
@@ -1,7 +1,22 @@
+
+mov ax, 3
+int 0x10 ; 将显示模式设置成文本模式
+
 mov ax, 0xb800
 mov ds, ax
 
-mov byte [0], 'T'
+mov byte [0], 'h'
+mov byte [2], 'e'
+mov byte [4], 'l'
+mov byte [6], 'l'
+mov byte [8], 'o'
+mov byte [10], ','
+mov byte [12], ' '
+mov byte [14], 'w'
+mov byte [16], 'o'
+mov byte [18], 'r'
+mov byte [20], 'l'
+mov byte [22], 'd'
 
 halt:
     jmp halt
