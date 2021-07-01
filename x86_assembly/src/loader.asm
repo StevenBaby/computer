0f820edf4fcf6400a0cace2874653fbaf2f45356
Steven Kang <kangweibaby@163.com>
2021-07-02 01:03:09 +0800

    x86 汇编语言 - 21 内存映射

--- x86_assembly/src/loader.asm
+++ x86_assembly/src/loader.asm
@@ -74,20 +74,74 @@ protect_enable:
 
     mov esp, 0x10000
 
-    mov ax, test_selector
-    mov gs, ax
+    xchg bx, bx
+
+    call setup_page
 
     xchg bx, bx
 
-    mov word [gs:0], 0x55aa
-
-    xchg bx, bx
-
-    mov word [gs:0x2000], 0x55aa
+    mov byte [0xC00b8000], 'P'
 
     xchg bx, bx
 
     jmp $
+
+PDE equ 0x2000
+PTE equ 0x3000
+ATTR equ 0b11
+
+setup_page:
+    mov eax, PDE
+    call .clear_page
+    mov eax, PTE
+    call .clear_page
+
+    ; 前面的 1M 内 前面的 1M
+    ; 前面的 1M 映射到 0xC0000000 ~ 0xC0100000
+    mov eax, PTE
+    or eax, ATTR
+    mov [PDE], eax; 
+    ;0b_00000_00000_00000_00000_00000_00000_00
+    mov [PDE + 0x300 * 4], eax;
+    ;0b_11000_00000_00000_00000_00000_00000_00
+
+    mov eax, PDE
+    or eax, ATTR
+    mov [PDE + 0x3ff * 4], eax; 吧最后一个页表指向了页目录
+
+    mov ebx, PTE
+    mov ecx, (0x100000 / 0x1000); 256
+    mov esi, 0
+    ; xchg bx, bx
+
+.next_page:
+    mov eax, esi
+    shl eax, 12
+    or eax, ATTR
+
+    mov [ebx + esi * 4], eax
+    inc esi
+    loop .next_page
+
+    ; xchg bx, bx
+    mov eax, PDE
+    mov cr3, eax
+
+    mov eax, cr0
+    or eax, 0b1000_0000_0000_0000_0000_0000_0000_0000
+    mov cr0, eax
+
+    ret
+
+.clear_page:
+    ; 清空一个内存页，地址参数存在 eax 中
+    mov ecx, 0x1000
+    mov esi, 0
+.set:
+    mov byte [eax + esi], 0
+    inc esi
+    loop .set
+    ret
 
 code_selector equ (1 << 3)
 data_selector equ (2 << 3)
